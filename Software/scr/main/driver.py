1fdd07f619fbe95468f8fad72bc5f5b3e098fcaf
JoaCieri <jcieri126@alumnos.frh.utn.edu.ar>
2026-05-22 16:52:51 -0300

    main y driver v1

--- Software/scr/main/driver.py
+++ Software/scr/main/driver.py
@@ -1,7 +1,82 @@
-# -*- coding: utf-8 -*-
-"""
-Editor de Spyder
+from pymodbus.client import ModbusTcpClient
 
-Este es un archivo temporal.
-"""
+class Driver:
 
+    def __init__(self):
+        self.client = None
+
+    def conectar(self):
+        self.client = ModbusTcpClient("192.168.0.50")
+
+        return self.client.connect()
+
+    def desconectar(self):
+        if self.client:
+            self.client.close()
+
+
+    # ---------------- LECTURA ----------------
+    def leer_datos(self):
+        if self.client is None:
+            print("No conectado")
+            return None
+
+        rr = self.client.read_holding_registers(address=0, count=31)
+
+        if rr.isError():
+            print("Error en lectura Modbus")
+            return None
+
+        r = rr.registers
+
+        # ----------- PARSEO -----------
+        datos = {
+            "L1": {
+                "Vrms": r[0] / 10,
+                "Irms": r[1] / 100,
+                "P": r[2] / 10,
+                "Q": r[3] / 10,
+                "S": r[4] / 10,
+                "FP": r[5] / 1000,
+                "THD_V": r[6] / 10,
+                "THD_I": r[7] / 10
+            },
+            "L2": {
+                "Vrms": r[8] / 10,
+                "Irms": r[9] / 100,
+                "P": r[10] / 10,
+                "Q": r[11] / 10,
+                "S": r[12] / 10,
+                "FP": r[13] / 1000,
+                "THD_V": r[14] / 10,
+                "THD_I": r[15] / 10
+            },
+            "L3": {
+                "Vrms": r[16] / 10,
+                "Irms": r[17] / 100,
+                "P": r[18] / 10,
+                "Q": r[19] / 10,
+                "S": r[20] / 10,
+                "FP": r[21] / 1000,
+                "THD_V": r[22] / 10,
+                "THD_I": r[23] / 10
+            },
+            "totales": {
+                "P_total": r[24] / 10,
+                "Q_total": r[25] / 10,
+                "S_total": r[26] / 10,
+                "FP_total": r[27] / 1000
+            },
+            "sistema": {
+                "frecuencia": r[28] / 10,
+                "topologia": r[29]
+            }
+        }
+
+        return datos
+
+    # ---------------- CERRAR ----------------
+    def desconectar(self):
+        if self.client:
+            self.client.close()
+            print("Conexión cerrada")
--- Software/scr/main/main.py
+++ Software/scr/main/main.py
@@ -1,7 +1,51 @@
 # -*- coding: utf-8 -*-
-"""
-Editor de Spyder
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import uic
 
-Este es un archivo temporal.
-"""
+from driver import Driver
 
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Cargar interfaz
+        uic.loadUi("MainWindow.ui", self)
+
+        # Crear driver
+        self.driver = Driver()
+
+        # Estado inicial
+        #self.label_estado.setText("Desconectado")
+
+        # Conectar botones (IMPORTANTE: nombres del .ui)
+        #self.btn_conectar.clicked.connect(self.conectar)
+        #self.btn_desconectar.clicked.connect(self.desconectar)
+
+    # ---------------- CONECTAR ----------------
+    def conectar(self):
+        self.label_estado.setText("Conectando...")
+
+        if self.driver.conectar():
+            self.label_estado.setText("Conectado - Leyendo...")
+        else:
+            self.label_estado.setText("Error de conexión")
+
+    # ---------------- DESCONECTAR ----------------
+    def desconectar(self):
+        self.driver.desconectar()
+        self.label_estado.setText("Desconectado")
+        
+    def cerrar(self):
+        pass
+
+
+# ---------------- MAIN ----------------
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
