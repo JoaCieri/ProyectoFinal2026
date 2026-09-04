4ea2a6d0bc8cc9e5dfb8ec5b19e8b1b13c6c8c4c
JoaCieri <jcieri126@alumnos.frh.utn.edu.ar>
2026-09-04 20:37:21 -0300

    Cambios interfaz

--- Software/scr/main/main.py
+++ Software/scr/main/main.py
@@ -18,29 +18,39 @@ class MainWindow(QMainWindow):
         self.driver = driver()
         self.historico = Historico()
 
-        # Estado inicialS
-        #self.label_estado.setText("Desconectado")
+        #Estado inicial
+        self.consola.setText("Desconectado")
+        self.prueba.setEnabled(False)
+        self.medicion.setEnabled(False)
 
         # Conectar botones (IMPORTANTE: nombres del .ui)
         self.btn_conectar.clicked.connect(self.conectar)
         self.btn_desconectar.clicked.connect(self.desconectar)
+        self.btn_salir.clicked.connect(self.cerrar)
+        self.prueba.clicked.connect(self.prueba)
+        self.prueba.clicked.connect(self.prueba)
 
     # ---------------- CONECTAR ----------------
     def conectar(self):
-        self.label_estado.setText("Conectando...")
+        self.consola.setText("Conectando...")
 
         if self.driver.conectar():
-            self.label_estado.setText("Conectado - Leyendo...")
+            self.consola.setText("Conectado - Leyendo...")
         else:
-            self.label_estado.setText("Error de conexión")
+            self.consola.setText("Error de conexión")
 
     # ---------------- DESCONECTAR ----------------
     def desconectar(self):
         self.driver.desconectar()
         self.label_estado.setText("Desconectado")
         
+    # ---------------- CERRAR LA GUI ----------------
+        
     def cerrar(self):
-        pass
+        if self.driver:
+            self.driver.desconectar()
+            
+        self.close()
 
 # ---------------- MAIN ----------------
 if __name__ == "__main__":
